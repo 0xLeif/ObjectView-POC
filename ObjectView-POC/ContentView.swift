fb1137b46958238e8d95292bab82faf468c99888
0xLeif <zmeriksen@icloud.com>
2020-11-07 11:45:19 -0600

    Updated to use SUIObject@0.4.0

--- ObjectView-POC/ContentView.swift
+++ ObjectView-POC/ContentView.swift
@@ -6,11 +6,24 @@
 //
 
 import SwiftUI
+import SUIObject
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Object("Hello, World!") { obj in
+            obj.add(variable: "otherText", value: "Another one")
+            obj.add(function: "change", value: { _ in
+                obj.variables["otherText"] = "\(obj.otherText.stringValue() ?? "")+"
+            })
+        }.view { obj in
+            VStack {
+                Text(obj.stringValue() ?? "-1")
+                Text(obj.otherText.stringValue() ?? "-2")
+                Button("Change") {
+                    obj.run(function: "change")
+                }
+            }
+        }
     }
 }
 
