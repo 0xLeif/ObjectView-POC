9e4dd22e407928c94d88b9d10bef3521d77f4f04
0xLeif <zmeriksen@icloud.com>
2020-11-07 11:31:58 -0600

    Initial Commit

---
+++ ObjectView-POC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ObjectView-POC
+//
+//  Created by Zach Eriksen on 11/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ObjectView-POC/ObjectView_POCApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectView_POCApp.swift
+//  ObjectView-POC
+//
+//  Created by Zach Eriksen on 11/7/20.
+//
+
+import SwiftUI
+
+@main
+struct ObjectView_POCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
